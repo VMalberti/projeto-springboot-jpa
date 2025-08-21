00bf74ac45ddfc6db97b8ad41dcae54eb26a4676
Vitor Cardoso <vitormalberti2005@gmail.com>
2025-08-21 17:48:11 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApplication {
+public class CourseApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(CourseApplication.class, args);
 	}
 
